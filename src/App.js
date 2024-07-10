2f6e71059ca853730bb669f02703654d253c1702
spydr-17 <phalkepratik17@gmail.com>
2024-07-10 23:06:18 +0530

    Initial frontend setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import AuctioneerPage from './pages/AuctioneerPage';
+import TeamPage from './pages/TeamPage';
+import SignupPage from './pages/SignupPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/auctioneer" component={AuctioneerPage} />
+      <Route path="/team" component={TeamPage} />
+      <Route path="/signup" component={SignupPage} />
+      <Route path="/login" component={LoginPage} />
+    </Routes>
   );
 }
 
