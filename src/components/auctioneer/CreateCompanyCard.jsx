8257e07746e3aaf805f98fc3cf57a5b2b036caba
hrishikeshtibile <hrishitibile@gmail.com>
2024-08-22 03:34:49 +0530

    fix: minor bug fix

--- src/components/auctioneer/CreateCompanyCard.jsx
+++ src/components/auctioneer/CreateCompanyCard.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const CreateCompanyForm = () => {
   const [formData, setFormData] = useState({
